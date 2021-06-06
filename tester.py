2d9a96115be02d440041b37262226b8f2ee3d0d1
Arek T <arkadiusztruszkowski94@gmail.com>
2021-06-06 06:51:44 -0400

    262 done

--- tester.py
+++ tester.py
@@ -1,9 +1,3 @@
-groceries = [{"product": "Milk", "quantity": 1, "price": 1.50},
-    {"product": "Eggs", "quantity": 12, "price": 0.10},
-    {"product": "Bread", "quantity": 2, "price": 1.60},
-    {"product": "Cheese", "quantity": 1, "price": 4.50}]
+cards = [{"color": "red", "number": 1, "shade": "lined", "shape": "squiggle"}, {"color": "red", "number": 1, "shade": "lined", "shape": "squiggle"}]
 
-
-combos = []
-combos.append([])
-print(combos)
+print(cards[0])
